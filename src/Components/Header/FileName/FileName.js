efe9d0ec214a768abb500e065f57d016190a30d9
abelmuro <abelmuro93@gmail.com>
2023-10-14 16:35:56 -0700

    commit

--- src/Components/Header/FileName/FileName.js
+++ src/Components/Header/FileName/FileName.js
@@ -2,20 +2,16 @@ import React, {useState} from 'react';
 import styles from './styles.module.css';
 import {useSelector} from 'react-redux';
 
-
-//need to fix the on change event handler
+//need to fix the change handler below
 function FileName() {
     const name = useSelector(state => state.file.name)    
     const [fileName, setFileName] = useState(name);
 
     const handleChange = (e) => {
-
-        console.log(e.target.value.match(/[^a-zA-Z._-]/g))
-        if(e.target.value.match(/[^a-zA-Z._-]/g)){
-            console.log('hi')
-            return;
-        } 
-        setFileName(e.target.value);
+        let input = e.target.value;
+        if(input.match(/[^a-zA-Z._-]/g)) return;
+    
+        setFileName(input);
     }
 
     return(
